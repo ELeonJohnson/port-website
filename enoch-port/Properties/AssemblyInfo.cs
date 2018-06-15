4bac165747b03295cfc36e14c51d523872329027
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-06-14 20:40:59 -0400

    practicing jquery and animations on new port site

---
+++ enoch-port/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("enoch_port")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("enoch_port")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7f8570ec-a032-442d-a5f0-3563a55f6488")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ enoch-port/scriptIndex.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $(".hide").hide();
+
+    $(".list-item").click(function () {
+        $("#nav").addClass("animated slideOutUp");
+        $("#nav").hide();
+    });
+
+    $("#about-click").click(function () {
+        $("body").css("background-color", "var(--grey)");
+        $("#about").addClass("animated slideInDown");
+        $("#about").show();
+    });
+
+    $("#portfolio-click").click(function () {
+        $("#portfolio").addClass("animated slideInUp");
+        $("body").css("background-color", "var(--black)");
+        $("#portfolio").show();
+    });
+
+    $("#skills-click").click(function () {
+        $("#skills").addClass("animated slideInUp");
+        $("body").css("background-color", "var(--tan)");
+        $("#skills").show();
+    });
+
+    $("#experience-click").click(function () {
+        $("#experience").addClass("animated slideInUp");
+        $("body").css("background-color", "var(--dark-blue)");
+        $("#experience").show();
+    });
+
+    $("#arrow-click").click(function () {
+        $("#about").hide();
+        $("#nav").show(); 
+        $("#nav").addClass("animated slideInDown");
+        
+    });
+
+  
+});
+
+
